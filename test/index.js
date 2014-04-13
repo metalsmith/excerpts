e19bbfc1c2bf9785305ef9d50a9f7a88776ba851
Shane Creighton-Young <srcreigh@uwaterloo.ca>
2014-04-13 19:15:10 -0400

    Fixed two edge cases: single paragraph and whitespace-leading pages

--- test/index.js
+++ test/index.js
@@ -5,11 +5,31 @@ var Metalsmith = require('metalsmith');
 
 describe('metalsmith-excerpts', function(){
   it('should convert excerpt files', function(done){
-    Metalsmith('test/fixture')
+    Metalsmith('test/fixtures/basic')
       .use(excerpt())
       .build(function(err, files){
         if (err) return done(err);
-        assert.equal('excerpt', files['index.md'].excerpt);
+        assert.equal('<p>excerpt</p>\n', files['index.md'].excerpt);
+        done();
+      });
+  });
+
+  it('should convert excerpt files that have leading whitespace', function(done){
+    Metalsmith('test/fixtures/whitespace')
+      .use(excerpt())
+      .build(function(err, files){
+        if (err) return done(err);
+        assert.equal('<p>excerpt</p>\n', files['index.md'].excerpt);
+        done();
+      });
+  });
+
+  it('should convert excerpt files that only have one paragraph', function(done){
+    Metalsmith('test/fixtures/one-paragraph')
+      .use(excerpt())
+      .build(function(err, files){
+        if (err) return done(err);
+        assert.equal('<p>excerpt</p>\n', files['index.md'].excerpt);
         done();
       });
   });
